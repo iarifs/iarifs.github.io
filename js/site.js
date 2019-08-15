4cc8ec6b5c59043cd6be11023f24d1ad2ba7bcb5
iarifs <arif.cvc13@gmail.com>
2019-08-15 14:49:18 -0500

    Resume added

--- js/site.js
+++ js/site.js
@@ -62,3 +62,21 @@ function scrollFunction() {
   }
 
 window.onscroll = function(){scrollFunction()};
+
+var msg  = document.title;
+var speed = 150;
+var pos = 0;
+
+function moveTitle()
+{
+     var ml = msg.length;
+        
+    title = msg.substr(pos,ml)  +" || "+ msg.substr(0,pos);
+  document.title = title;
+    
+  pos++;
+  if (pos > ml) pos=0;
+  window.setTimeout("moveTitle()",speed);
+}
+
+moveTitle();
